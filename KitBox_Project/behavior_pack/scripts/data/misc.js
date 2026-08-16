95662847fdec364136dcbdbc0100e7f1fabc633a
noob66666666 <111114@stu.bjes.tp.edu.tw>
2026-08-16 08:59:18 +0800

    Fix KitBox supply quantities to match requirements

--- KitBox_Project/behavior_pack/scripts/data/misc.js
+++ KitBox_Project/behavior_pack/scripts/data/misc.js
@@ -1,9 +1,9 @@
 export const MISC = [
+  { id:'steak', item:'minecraft:cooked_beef', amount:64 },
   { id:'golden_apple', item:'minecraft:golden_apple', amount:64 },
   { id:'enchanted_golden_apple', item:'minecraft:enchanted_golden_apple', amount:64 },
-  { id:'cooked_beef', item:'minecraft:cooked_beef', amount:64 },
   { id:'arrow', item:'minecraft:arrow', amount:64 },
-  { id:'ender_pearl', item:'minecraft:ender_pearl', amount:16 },
+  { id:'ender_pearl', item:'minecraft:ender_pearl', amount:64 },
   { id:'totem_of_undying', item:'minecraft:totem_of_undying', amount:10 },
   { id:'water_bucket', item:'minecraft:water_bucket', amount:1 },
   { id:'crafting_table', item:'minecraft:crafting_table', amount:1 },
