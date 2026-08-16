2b610d8ce00a9ae1d0390e262fe4a44b8d5ed87c
noob66666666 <111114@stu.bjes.tp.edu.tw>
2026-08-16 09:32:59 +0800

    refactor: define stable KitBox delivery order

--- KitBox_Project/behavior_pack/scripts/core/equipment_sets.js
+++ KitBox_Project/behavior_pack/scripts/core/equipment_sets.js
@@ -2,4 +2,16 @@ import { ARMOR } from "../data/armor.js";
 import { WEAPONS } from "../data/weapons.js";
 import { TOOLS } from "../data/tools.js";
 import { MISC } from "../data/misc.js";
-export const EQUIPMENT_SETS=[...ARMOR,...WEAPONS,...TOOLS,...MISC];
+
+// Keep a stable delivery order so the player receives combat gear first,
+// then tools, then supplies. Armor remains grouped by slot and protection set.
+export const EQUIPMENT_SETS = Object.freeze([
+  ...WEAPONS,
+  ...TOOLS,
+  ...MISC,
+  ...ARMOR,
+]);
+
+export function getEquipmentDefinitions() {
+  return EQUIPMENT_SETS;
+}
