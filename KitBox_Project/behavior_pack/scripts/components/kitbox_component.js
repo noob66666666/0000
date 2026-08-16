67f491466a55fac579365a446088ec2635712208
noob66666666 <111114@stu.bjes.tp.edu.tw>
2026-08-16 08:50:58 +0800

    Fix KitBox custom component registration

--- KitBox_Project/behavior_pack/scripts/components/kitbox_component.js
+++ KitBox_Project/behavior_pack/scripts/components/kitbox_component.js
@@ -1,1 +1,12 @@
-export function registerKitBoxComponent(){ return true; }
+import { useKitBox } from "../core/kitbox_logic.js";
+
+export const KITBOX_COMPONENT_ID = "kitbox:use_kitbox";
+
+export function registerKitBoxComponent(itemComponentRegistry) {
+  itemComponentRegistry.registerCustomComponent(KITBOX_COMPONENT_ID, {
+    onUse(event) {
+      if (!event.source) return;
+      useKitBox(event.source);
+    }
+  });
+}
