85848a8f80e0b6acde742b848c7f66f79ce05d46
noob66666666 <111114@stu.bjes.tp.edu.tw>
2026-08-16 08:48:46 +0800

    Phase 24: 完整化武器資料

--- KitBox_Project/behavior_pack/scripts/data/weapons.js
+++ KitBox_Project/behavior_pack/scripts/data/weapons.js
@@ -1,5 +1,68 @@
-export const WEAPONS=[
-{id:'sword',item:'minecraft:netherite_sword',count:1,enchantments:[['sharpness',5],['looting',3],['fire_aspect',2],['unbreaking',3],['mending',1]]},
-{id:'bow_infinity',item:'minecraft:bow',count:1,enchantments:[['power',5],['flame',1],['infinity',1],['unbreaking',3]]},
-{id:'bow_mending',item:'minecraft:bow',count:1,enchantments:[['power',5],['flame',1],['mending',1],['unbreaking',3]]}
+export const WEAPONS = [
+  {
+    id: 'sword_sharpness',
+    item: 'minecraft:netherite_sword',
+    count: 1,
+    enchantments: [['sharpness', 5], ['looting', 3], ['fire_aspect', 2], ['unbreaking', 3], ['mending', 1]]
+  },
+  {
+    id: 'sword_smite',
+    item: 'minecraft:netherite_sword',
+    count: 1,
+    enchantments: [['smite', 5], ['looting', 3], ['fire_aspect', 2], ['unbreaking', 3], ['mending', 1]]
+  },
+  {
+    id: 'sword_bane_of_arthropods',
+    item: 'minecraft:netherite_sword',
+    count: 1,
+    enchantments: [['bane_of_arthropods', 5], ['looting', 3], ['fire_aspect', 2], ['unbreaking', 3], ['mending', 1]]
+  },
+  {
+    id: 'bow_infinity',
+    item: 'minecraft:bow',
+    count: 1,
+    enchantments: [['power', 5], ['flame', 1], ['infinity', 1], ['unbreaking', 3]]
+  },
+  {
+    id: 'bow_mending',
+    item: 'minecraft:bow',
+    count: 1,
+    enchantments: [['power', 5], ['flame', 1], ['mending', 1], ['unbreaking', 3]]
+  },
+  {
+    id: 'crossbow_multishot',
+    item: 'minecraft:crossbow',
+    count: 1,
+    enchantments: [['multishot', 1], ['quick_charge', 3], ['unbreaking', 3], ['mending', 1]]
+  },
+  {
+    id: 'crossbow_piercing',
+    item: 'minecraft:crossbow',
+    count: 1,
+    enchantments: [['piercing', 4], ['quick_charge', 3], ['unbreaking', 3], ['mending', 1]]
+  },
+  {
+    id: 'trident_loyalty',
+    item: 'minecraft:trident',
+    count: 1,
+    enchantments: [['loyalty', 3], ['impaling', 5], ['channeling', 1], ['unbreaking', 3], ['mending', 1]]
+  },
+  {
+    id: 'trident_riptide',
+    item: 'minecraft:trident',
+    count: 1,
+    enchantments: [['riptide', 3], ['impaling', 5], ['unbreaking', 3], ['mending', 1]]
+  },
+  {
+    id: 'axe_combat',
+    item: 'minecraft:netherite_axe',
+    count: 1,
+    enchantments: [['sharpness', 5], ['efficiency', 5], ['unbreaking', 3], ['mending', 1]]
+  },
+  {
+    id: 'axe_tool',
+    item: 'minecraft:netherite_axe',
+    count: 1,
+    enchantments: [['efficiency', 5], ['silk_touch', 1], ['unbreaking', 3], ['mending', 1]]
+  }
 ];
