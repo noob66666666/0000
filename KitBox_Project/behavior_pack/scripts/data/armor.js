ea817610b53dc36aead829e58604853928bfd1e1
noob66666666 <111114@stu.bjes.tp.edu.tw>
2026-08-16 08:42:26 +0800

    Phase23: complete non-weapon armor variants

--- KitBox_Project/behavior_pack/scripts/data/armor.js
+++ KitBox_Project/behavior_pack/scripts/data/armor.js
@@ -1,6 +1,53 @@
-export const ARMOR=[
-{id:'helmet_protection',item:'minecraft:netherite_helmet',count:1,enchantments:[['protection',4],['unbreaking',3],['mending',1],['respiration',3],['aqua_affinity',1]]},
-{id:'helmet_fire',item:'minecraft:netherite_helmet',count:1,enchantments:[['fire_protection',4],['unbreaking',3],['mending',1],['respiration',3],['aqua_affinity',1]]},
-{id:'helmet_blast',item:'minecraft:netherite_helmet',count:1,enchantments:[['blast_protection',4],['unbreaking',3],['mending',1],['respiration',3],['aqua_affinity',1]]},
-{id:'helmet_projectile',item:'minecraft:netherite_helmet',count:1,enchantments:[['projectile_protection',4],['unbreaking',3],['mending',1],['respiration',3],['aqua_affinity',1]]}
+const COMMON = [['unbreaking', 3], ['mending', 1]];
+const HEAD = [['respiration', 3], ['aqua_affinity', 1]];
+const PROTECTION = [
+  ['protection', 4, 'protection'],
+  ['fire_protection', 4, 'fire'],
+  ['blast_protection', 4, 'blast'],
+  ['projectile_protection', 4, 'projectile'],
+];
+
+function enchants(primary, extra = []) {
+  return [primary, ...extra, ...COMMON];
+}
+
+export const ARMOR = [
+  ...PROTECTION.map(([type, level, id]) => ({
+    id: `helmet_${id}`,
+    item: 'minecraft:netherite_helmet',
+    count: 1,
+    enchantments: enchants([type, level], HEAD),
+  })),
+  ...PROTECTION.map(([type, level, id]) => ({
+    id: `chestplate_${id}`,
+    item: 'minecraft:netherite_chestplate',
+    count: 1,
+    enchantments: enchants([type, level]),
+  })),
+  ...PROTECTION.map(([type, level, id]) => ({
+    id: `leggings_${id}`,
+    item: 'minecraft:netherite_leggings',
+    count: 1,
+    enchantments: enchants([type, level], [['swift_sneak', 3]]),
+  })),
+  ...PROTECTION.flatMap(([type, level, id]) => [
+    {
+      id: `boots_${id}_depth_strider`,
+      item: 'minecraft:netherite_boots',
+      count: 1,
+      enchantments: enchants([type, level], [['feather_falling', 4], ['depth_strider', 3]]),
+    },
+    {
+      id: `boots_${id}_frost_walker`,
+      item: 'minecraft:netherite_boots',
+      count: 1,
+      enchantments: enchants([type, level], [['feather_falling', 4], ['frost_walker', 2]]),
+    },
+    {
+      id: `boots_${id}_soul_speed`,
+      item: 'minecraft:netherite_boots',
+      count: 1,
+      enchantments: enchants([type, level], [['feather_falling', 4], ['depth_strider', 3], ['soul_speed', 3]]),
+    },
+  ]),
 ];
