a83dc0346fd379b9e203564fbe15ff3fcb4a4239
noob66666666 <111114@stu.bjes.tp.edu.tw>
2026-08-17 09:19:05 +0800

    Align KitBox use handler with current item component API

--- KitBox_Project/behavior_pack/scripts/components/kitbox_component.js
+++ KitBox_Project/behavior_pack/scripts/components/kitbox_component.js
@@ -1,3 +1,4 @@
+import { ItemComponentUseEvent } from "@minecraft/server";
 import { useKitBox } from "../core/kitbox_logic.js";
 
 export const KITBOX_COMPONENT_ID = "kitbox:use_kitbox";
@@ -9,7 +10,8 @@ export function registerKitBoxComponent(itemComponentRegistry) {
 
   itemComponentRegistry.registerCustomComponent(KITBOX_COMPONENT_ID, {
     onUse(event) {
-      const player = event?.source;
+      const useEvent = event;
+      const player = useEvent.source;
       if (!player) return;
       useKitBox(player);
     }
