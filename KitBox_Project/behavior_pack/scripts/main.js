69e74a632928d6eb1e85c12b982c43d0a89cbe4d
noob66666666 <111114@stu.bjes.tp.edu.tw>
2026-08-18 10:14:23 +0800

    Run temporary native shulker verification on load

--- KitBox_Project/behavior_pack/scripts/main.js
+++ KitBox_Project/behavior_pack/scripts/main.js
@@ -1,6 +1,12 @@
 import { system } from "@minecraft/server";
 import { registerKitBoxComponent } from "./components/kitbox_component.js";
+import { scheduleShulkerProbe } from "./core/shulker_probe.js";
 
 system.beforeEvents.startup.subscribe((initEvent) => {
   registerKitBoxComponent(initEvent.itemComponentRegistry);
 });
+
+// Temporary native-shulker verification. Remove after the iOS manual test passes.
+system.runTimeout(() => {
+  scheduleShulkerProbe();
+}, 40);
